12a32343e7fa8542f62a0a0ea038b8e59897ce31
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-01-12 21:07:15 -0500

    Initial commit

---
+++ w1/Population.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Population {
+	public static void main(String[] args) {
+		
+		/*
+		// 1
+		System.out.println("Population projection");
+		System.out.println("Current population: 312032486");
+		System.out.println("One birth every 7 seconds.");
+		System.out.println("One death every 13 seconds.");
+		System.out.println("One new immigrant every 45 seconds.");
+		System.out.println("Yearly Population projection formula: ");
+		
+		int second=365*24*60*60;
+		int incrementRatio=(second/7)-(second/13)+(second/45);
+		int result=312032486;
+		
+		for(int i=1;i<6;i++)
+		{
+			result+=incrementRatio;
+			System.out.println(i+"year population: "+result);
+		}
+		
+		// 2
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter a number between 0 and 1000: ");
+		int num=input.nextInt();
+		
+		int firstNum=num/100;
+		int secondNum=(num/10)%10;
+		int thirdNum=num%10;
+		System.out.println(firstNum+" , "+secondNum+" , "+thirdNum);
+		int resultTwo=firstNum+secondNum+thirdNum;
+		System.out.println("The sum of the digits is "+resultTwo);
+		
+		// 3
+		//final double MONTHLY_INTEREST_RATE = 0.00417;
+		
+		
+		//4
+		int month=(int)((Math.random()*12)+1);
+		
+		switch(month)
+		{
+		case 1: System.out.println("January"); break;
+		case 2: System.out.println("February"); break;
+		case 3: System.out.println("March"); break;
+		case 4: System.out.println("April"); break;
+		case 5: System.out.println("May"); break;
+		case 6: System.out.println("June"); break;
+		case 7: System.out.println("July"); break;
+		case 8: System.out.println("August"); break;
+		case 9: System.out.println("September"); break;
+		case 10: System.out.println("October"); break;
+		case 11: System.out.println("November"); break;
+		case 12: System.out.println("December");
+		}
+		
+		//5
+		 
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
