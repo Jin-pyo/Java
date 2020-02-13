748c62960fe985646b647a40d7b444ffad96d867
Jin-pyo <48873560+Jin-pyo@users.noreply.github.com>
2020-02-13 10:35:05 -0500

    create word & set aster

--- workshop/workshop4/Task2/src/Hangman.java
+++ workshop/workshop4/Task2/src/Hangman.java
@@ -2,15 +2,35 @@
 public class Hangman {
 	
 	private String[] words= {"apple","bee","power","pig","rabbit"};
+	private char[] word;
 	
-	public String getword() {
-		
+	public Hangman(){
 		int number=(int)(Math.random()*words.length)+1;
 		System.out.println(number);
-		int n=words.length;
-		System.out.println(n);
+		this.word=new char[words[number-1].length()];
 		
-		return this.words[number];
+		for(int i=0;i<word.length;i++)
+		{
+			word[i]=words[number-1].charAt(i);
+		}
 	}
+	
+	public char[] getword() {
+		
+		return word;
+		
+	}
+	
+	public void fillasteriks(char[] list)
+	{
+		for(int i=0;i<list.length;i++)
+		{
+			list[i]='*';
+			System.out.print(list[i]);
+		}
+		System.out.println(" ");
+	}
+	
+	
 	
 }
--- workshop/workshop4/Task2/src/Task2.java
+++ workshop/workshop4/Task2/src/Task2.java
@@ -4,19 +4,49 @@ public class Task2 {
 
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		Hangman hang = new Hangman();
+		Scanner scanner = new Scanner(System.in);
+		
+		
 		boolean playLoopcheck=true;
 		
-		String a=hang.getword();
-		
-		System.out.println(a);
-		
-		/*
 		while(playLoopcheck)
 		{
+			System.out.println("Do you want to play Game? 1:Yes , 2:No ");
 			
+			int playcheck=scanner.nextInt();
+			
+			if(playcheck==2)
+			{
+				System.out.println("Bye Bye");
+				playLoopcheck=false;
+			}
+			else
+			{
+				Hangman hangman = new Hangman();
+				char[] word=hangman.getword();
+				char[] asterword=new char[word.length];
+				hangman.fillasteriks(asterword);
+				
+				boolean gameLoopCheck=true;
+				
+				while(gameLoopCheck)
+				{
+					System.out.print("(Guess) Enter a letter in word ");
+					System.out.print(asterword);
+					System.out.print(" > ");
+					//char guessWord=scanner.next().charAt(0);
+					
+					System.out.println(hangman.getword());
+					gameLoopCheck=false;
+					
+					
+					
+					
+					
+				}
+				
+			}
 		}
-		*/
+		
 	}
 }
